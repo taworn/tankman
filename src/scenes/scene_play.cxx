d29ff5b6ae8fc62a1abf48cada09b36311ae0511
Taworn T <taworn@gmail.com>
2016-10-05 10:57:42 +0700

    - Use keyboard state instead pull event to make keyboard logic faster.

--- src/scenes/scene_play.cxx
+++ src/scenes/scene_play.cxx
@@ -18,6 +18,7 @@ ScenePlay::~ScenePlay()
 
 ScenePlay::ScenePlay()
 	: arena(Game::instance()->getArena())
+	, keyTime(0)
 {
 	SDL_Log("ScenePlay::ScenePlay()");
 	arena->startBattle();
@@ -25,36 +26,59 @@ ScenePlay::ScenePlay()
 
 bool ScenePlay::handleKey(SDL_KeyboardEvent key)
 {
-	SDL_Log("ScenePlay::handleKey(%d)", key.keysym.sym);
+#ifdef __ANDROID__
 	if (key.keysym.sym == SDLK_SPACE || key.keysym.sym == SDLK_RETURN) {
 		arena->getMap()->getHero()->fire();
 		return true;
 	}
-	else if (key.keysym.sym == SDLK_a || key.keysym.sym == SDLK_LEFT) {
-		SDL_Log("press left");
+	if (key.keysym.sym == SDLK_a || key.keysym.sym == SDLK_LEFT) {
 		arena->getMap()->getHero()->move(Movable::MOVE_LEFT);
 		return true;
 	}
 	else if (key.keysym.sym == SDLK_d || key.keysym.sym == SDLK_RIGHT) {
-		SDL_Log("press right");
 		arena->getMap()->getHero()->move(Movable::MOVE_RIGHT);
 		return true;
 	}
 	else if (key.keysym.sym == SDLK_w || key.keysym.sym == SDLK_UP) {
-		SDL_Log("press up");
 		arena->getMap()->getHero()->move(Movable::MOVE_UP);
 		return true;
 	}
 	else if (key.keysym.sym == SDLK_s || key.keysym.sym == SDLK_DOWN) {
-		SDL_Log("press down");
 		arena->getMap()->getHero()->move(Movable::MOVE_DOWN);
 		return true;
 	}
+#endif
 	return false;
 }
 
 void ScenePlay::render(int timeUsed)
 {
+#ifndef __ANDROID__
+	const Uint8 *states = SDL_GetKeyboardState(NULL);
+	keyTime += timeUsed;
+	if (keyTime > 100) {
+		if (states[SDL_SCANCODE_SPACE] || states[SDL_SCANCODE_RETURN]) {
+			arena->getMap()->getHero()->fire();
+		}
+		if (states[SDL_SCANCODE_A] || states[SDL_SCANCODE_LEFT]) {
+			arena->getMap()->getHero()->move(Movable::MOVE_LEFT);
+			keyTime = 0;
+		}
+		else if (states[SDL_SCANCODE_D] || states[SDL_SCANCODE_RIGHT]) {
+			arena->getMap()->getHero()->move(Movable::MOVE_RIGHT);
+			keyTime = 0;
+		}
+		else if (states[SDL_SCANCODE_W] || states[SDL_SCANCODE_UP]) {
+			arena->getMap()->getHero()->move(Movable::MOVE_UP);
+			keyTime = 0;
+		}
+		else if (states[SDL_SCANCODE_S] || states[SDL_SCANCODE_DOWN]) {
+			arena->getMap()->getHero()->move(Movable::MOVE_DOWN);
+			keyTime = 0;
+		}
+	}
+#endif
+
 	arena->draw(timeUsed);
 }
 
--- src/scenes/scene_play.hxx
+++ src/scenes/scene_play.hxx
@@ -19,6 +19,7 @@ public:
 
 private:
 	Arena *arena;
+	int keyTime;
 };
 
 #endif // SCENES_SCENE_PLAY_HXX
